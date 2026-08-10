28d41c6630096ec849a55d4e8f50ac8c920a5c13
iki1uc <entertrainer@web.de>
2026-08-10 13:47:04 +0200

    Update 3mal3.js

--- 3mal3.js
+++ 3mal3.js
@@ -1,20 +1,3 @@
-(Matrix)
-Realinhalt:
-
 a = Fläche
-
 b = Körper
-
 c = Continuum
-
-3×3 Matrizen
-
-c = a × b × t
-
-Interpretation (rein real):
-
-3×3 ist die Grundstruktur
-
-c ist dynamisch
-
-a/b sind statisch
