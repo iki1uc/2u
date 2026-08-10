0f462afd8533f39e0e70a7c5f0c82999d027e171
iki1uc <entertrainer@web.de>
2026-08-10 13:56:01 +0200

    Update and rename 6D.html to 6D.js

---
+++ 6D.js
@@ -0,0 +1,19 @@
+1D: Rotation (360.js)
+2D: Matrix (3mal3.js)
+3D: Ursache (KIpi.U)
+4D: Wirkung (KIpi.W)
+5D: Ergebnis (KIpi.E / MAIN)
+6D: Summation (OUR.js / ICOME)
+4 → 5 → 6 → 7 → 8 → 9
+W1 → W2 → W3 → E1 → E2 → E3
+0 → 1 → 2
+a → b → c
+grün → gelb → rot
+VTALITY = TMP(main) + TMP(home) + TMP(ort)
+tmp(zu(tmp(erreignis)))
+Sonne = c (rot)
+Planeten = a + b
+c = a × b × t
+ICOME = Σ(a + b + c)
+Σ◉◎▣
+iki1uc wieimmer
