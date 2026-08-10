277b8feaf7bde958b37607472b2c9e4ec0b6cfbc
iki1uc <entertrainer@web.de>
2026-08-10 13:31:27 +0200

    Create 360.js

---
+++ 360.js
@@ -0,0 +1,18 @@
+(Rotation)
+Realinhalt:
+
+360°
+
+8×
+
+12×
+
+virtuelle Adressen
+
+TMP360
+
+Interpretation (rein real):
+
+360.js ist die Rotations‑Geometrie
+
+TMP360 ist die Rotations‑Führung
