999c1e614e3441bb081851459c88eb368a4a91cc
iki1uc <entertrainer@web.de>
2026-08-10 13:30:36 +0200

    Create OUR.js

---
+++ OUR.js
@@ -0,0 +1,26 @@
+(Universal‑Modul)
+Realinhalt:
+
+Prefetch a/b/c
+
+Bildung BIL/DU/NG
+
+Matrix a/b/c
+
+KIpi U/W/E
+
+TMP 1–9
+
+Materialstand
+
+Nutzwert
+
+SUM
+
+Interpretation (rein real):
+
+OUR.js ist der Universal‑Interpreter
+
+Er verbindet alle RAW‑Dateien
+
+Er liefert die Summation
