dfdf0b0c31bf84b0472eeee6fad7bea1a727f97b
iki1uc <entertrainer@web.de>
2026-08-10 13:46:52 +0200

    Update OUR.js

--- OUR.js
+++ OUR.js
@@ -1,26 +1,3 @@
-(Universal‑Modul)
-Realinhalt:
-
-Prefetch a/b/c
-
-Bildung BIL/DU/NG
-
-Matrix a/b/c
-
-KIpi U/W/E
-
-TMP 1–9
-
-Materialstand
-
-Nutzwert
-
-SUM
-
-Interpretation (rein real):
-
-OUR.js ist der Universal‑Interpreter
-
-Er verbindet alle RAW‑Dateien
-
-Er liefert die Summation
+BIL = a = 0
+DU  = b = 1
+NG  = c = 2
